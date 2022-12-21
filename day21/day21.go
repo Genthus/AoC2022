d1301bf7850c706656926af32f8754df00ae00af
Genthus <solracescalante@gmail.com>
2022-12-21 11:55:28 -0600

    day21 complete!

--- day21/day21.go
+++ day21/day21.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"errors"
-	"fmt"
 	"log"
 	"strconv"
 	"strings"
@@ -16,37 +15,122 @@ type operation struct {
 }
 
 func operate(o *operation, valueMap map[string]int, operationMap map[string]*operation) (int, error) {
-	for {
-		if _, ok := valueMap[o.value1]; !ok {
-			if op, ok := operationMap[o.value1]; ok {
-				valueMap[o.value1], _ = operate(op, valueMap, operationMap)
-			} else {
-				return 0, errors.New("not found in operationMap")
-			}
-		}
-		if _, ok := valueMap[o.value2]; !ok {
-			if op, ok := operationMap[o.value2]; ok {
-				valueMap[o.value2], _ = operate(op, valueMap, operationMap)
-			} else {
-				return 0, errors.New("not found in operationMap")
-			}
-		}
-		v1, v2 := valueMap[o.value1], valueMap[o.value2]
-		fmt.Println(o.name, ":", o.value1, v1, o.operation, o.value2, v2)
-
-		switch o.operation {
-		case "+":
-			return v1 + v2, nil
-		case "-":
-			return v1 - v2, nil
-		case "*":
-			return v1 * v2, nil
-		case "/":
-			return v1 / v2, nil
-		default:
-			return 0, errors.New("unknown operation")
+	if _, ok := valueMap[o.value1]; !ok {
+		if op, ok := operationMap[o.value1]; ok {
+			valueMap[o.value1], _ = operate(op, valueMap, operationMap)
+		} else {
+			return 0, errors.New("not found in operationMap")
 		}
 	}
+	if _, ok := valueMap[o.value2]; !ok {
+		if op, ok := operationMap[o.value2]; ok {
+			valueMap[o.value2], _ = operate(op, valueMap, operationMap)
+		} else {
+			return 0, errors.New("not found in operationMap")
+		}
+	}
+	v1, v2 := valueMap[o.value1], valueMap[o.value2]
+
+	switch o.operation {
+	case "+":
+		return v1 + v2, nil
+	case "-":
+		return v1 - v2, nil
+	case "*":
+		return v1 * v2, nil
+	case "/":
+		return v1 / v2, nil
+	default:
+		return 0, errors.New("unknown operation")
+	}
+}
+
+func unoperate(o *operation, valueMap map[string]int, operationMap map[string]*operation) (int, []string) {
+	influence := []string{}
+	if _, ok := valueMap[o.value1]; !ok {
+		if op, ok := operationMap[o.value1]; ok {
+			pi := []string{}
+			valueMap[o.value1], pi = unoperate(op, valueMap, operationMap)
+			if len(pi) > 0 {
+				influence = pi
+			}
+		} else {
+			return 0, nil
+		}
+	}
+	if _, ok := valueMap[o.value2]; !ok {
+		if op, ok := operationMap[o.value2]; ok {
+			pi := []string{}
+			valueMap[o.value2], pi = unoperate(op, valueMap, operationMap)
+			if len(pi) > 0 {
+				influence = pi
+			}
+		} else {
+			return 0, nil
+		}
+	}
+	if len(influence) > 0 || o.value1 == "humn" || o.value2 == "humn" {
+		if o.value1 == "humn" || o.value2 == "humn" {
+			influence = append(influence, "humn")
+		}
+		return 0, append(influence, o.name)
+	} else if o.name == "root" {
+		influence = append(influence, o.name)
+	}
+	v1, v2 := valueMap[o.value1], valueMap[o.value2]
+
+	switch o.operation {
+	case "+":
+		return v1 + v2, influence
+	case "-":
+		return v1 - v2, influence
+	case "*":
+		return v1 * v2, influence
+	case "/":
+		return v1 / v2, influence
+	default:
+		return 0, nil
+	}
+}
+
+func dispelMystery(valueMap map[string]int, operationMap map[string]*operation, influence []string) int {
+	currentNode := influence[len(influence)-1]
+	var eq int
+	for len(influence) > 0 {
+		if currentNode == "humn" {
+			break
+		}
+		var nodeR string
+		n := operationMap[influence[len(influence)-1]]
+		rhs := false
+		if n.value1 == influence[len(influence)-2] {
+			nodeR = n.value2
+		} else {
+			nodeR = n.value1
+			rhs = true
+		}
+		if currentNode == "root" {
+			eq = valueMap[nodeR]
+		} else {
+			switch operationMap[currentNode].operation {
+			case "+":
+				eq -= valueMap[nodeR]
+			case "-":
+				if rhs {
+					eq = valueMap[nodeR] - eq
+				} else {
+					eq += valueMap[nodeR]
+				}
+			case "*":
+				eq /= valueMap[nodeR]
+			case "/":
+				eq *= valueMap[nodeR]
+			}
+		}
+		influence = influence[:len(influence)-1]
+		currentNode = influence[len(influence)-1]
+	}
+	return eq
 }
 
 type value struct {
@@ -56,12 +140,10 @@ type value struct {
 
 func ParseString(input string) (*operation, *value) {
 	parts := strings.Split(input, ": ")
-	fmt.Println(parts)
 	name := parts[0]
 	data := parts[1]
 	if strings.ContainsAny(data, "*/+-") {
 		opData := strings.Split(data, " ")
-		fmt.Println("optData", opData)
 		return &operation{name, opData[0], opData[2], opData[1]}, nil
 	} else {
 		num, err := strconv.Atoi(data)
@@ -125,13 +207,9 @@ func MonkeyBrain(input []string) int {
 			if more {
 				o, v := ParseString(j)
 				if v != nil {
-					if v.name != "humn" {
-						valueMap[v.name] = v.value
-					}
+					valueMap[v.name] = v.value
 				} else if o != nil {
-					if o.name != "humn" {
-						operationMap[o.name] = o
-					}
+					operationMap[o.name] = o
 				}
 			} else {
 				done <- true
@@ -148,9 +226,7 @@ func MonkeyBrain(input []string) int {
 	close(jobs)
 
 	<-done
-	res, err := operate(operationMap["root"], valueMap, operationMap)
-	if err != nil {
-		log.Fatal(err)
-	}
+	res, hmn := unoperate(operationMap["root"], valueMap, operationMap)
+	res = dispelMystery(valueMap, operationMap, hmn)
 	return res
 }
--- day21/day21_test.go
+++ day21/day21_test.go
@@ -30,6 +30,29 @@ hmdt: 32
 		t.Error("Expected 152, got", res)
 	}
 }
+func TestExample2(t *testing.T) {
+	rawInput := `root: pppw + sjmn
+dbpl: 5
+cczh: sllz + lgvd
+zczc: 2
+ptdq: humn - dvpt
+dvpt: 3
+lfqf: 4
+humn: 5
+ljgn: 2
+sjmn: drzm * dbpl
+sllz: 4
+pppw: cczh / lfqf
+lgvd: ljgn * ptdq
+drzm: hmdt - zczc
+hmdt: 32
+`
+	input := strings.Split(rawInput, "\n")
+	res := MonkeyBrain(input)
+	if res != 301 {
+		t.Error("Expected 301, got", res)
+	}
+}
 
 func TestInput(t *testing.T) {
 	rawInput, err := os.ReadFile("input")
@@ -38,5 +61,15 @@ func TestInput(t *testing.T) {
 	}
 	input := strings.Split(string(rawInput), "\n")
 	res := QueueChecks(input)
-	fmt.Println("Result:", res)
+	fmt.Println("Part 1 Result:", res)
+}
+
+func TestInputPart2(t *testing.T) {
+	rawInput, err := os.ReadFile("input")
+	if err != nil {
+		t.Fatal(err)
+	}
+	input := strings.Split(string(rawInput), "\n")
+	res := MonkeyBrain(input)
+	fmt.Println("Part 2 Result:", res)
 }
